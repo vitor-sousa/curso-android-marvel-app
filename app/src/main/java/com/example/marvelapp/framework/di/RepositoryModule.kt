b65508117383c404edc5cb8a934d77fad096ee3a
Vitor Sousa <vitoramaromoraes@gmail.com>
2022-11-18 20:48:00 -0300

    - implement paging - implement repository DI

---
+++ app/src/main/java/com/example/marvelapp/framework/di/RepositoryModule.kt
@@ -0,0 +1,26 @@
+package com.example.marvelapp.framework.di
+
+import com.example.core.data.repository.CharacterRepository
+import com.example.core.data.repository.CharactersRemoteDataSource
+import com.example.marvelapp.framework.CharactersRepositoryImpl
+import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.example.marvelapp.framework.remote.RetrofitCharactersDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+
+    @Binds
+    fun bindCharacterRepository(
+        repository: CharactersRepositoryImpl
+    ): CharacterRepository
+
+    @Binds
+    fun bindRemoteDataSource(
+        dataSource: RetrofitCharactersDataSource
+    ): CharactersRemoteDataSource<DataWrapperResponse>
+}
